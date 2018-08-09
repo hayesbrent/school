c138bb9e78935427a641ec3eeca3877d8f837f73
hayesbrent <forgottenpetfish@gmail.com>
2018-08-09 11:14:57 -0700

    movement mapped

--- final.cpp
+++ final.cpp
@@ -31,7 +31,7 @@ void floor3a(playerInfo &, int [10]);
 void floor3b(playerInfo &, int [10]);
 void floor3c(playerInfo &, int [10]);
 void floor3(playerInfo &, int [10]);
-int playerDirection();
+int playerDirection(int, int, int, int, playerInfo &);
 void playerInputValidation(playerInfo &, int);
 
 void introduction(playerInfo &);
@@ -54,6 +54,9 @@ int main() {
     
 
     introduction(player);
+    floor3(player,roomstate);
+
+    cout << "Amazing job," << player.name << ", you are a hero among heroes! You have made it out of the dungeon!!!" << endl;
    
     
 
@@ -111,8 +114,6 @@ void playerInputValidation (playerInfo &player, int set) {
             }
         }
     }
-
-
 }
 
 
@@ -128,33 +129,174 @@ void introduction(playerInfo &player) {
 
 
 void floor3(playerInfo &player, int roomstate[8]) {
+    int playerMovement = 0;
+    if ( roomstate[0] == 0) {
     cout << "You peer into the darkness of the room, and suddenly a specter appears before you. His wide smile sends chills down to your soul. \"What is your name adventurer\?\"" << endl;
     cout << "1 - State your name proudly\n2 - Keep wary of the ghastly ghoul and say nothing" << endl;
     playerInputValidation(player, 30);
-
+     roomstate[0] = 1;
+    }
+    playerMovement = playerDirection(1,0,1,1,player);
+    if (playerMovement == 1) {
+        floor3b(player, roomstate);
+    } else if (playerMovement == 3) {
+        floor3c(player, roomstate);
+    } else if (playerMovement == 4) {
+        floor3a(player, roomstate);
+    }
 }
 void floor3a(playerInfo &player, int roomstate[8]) {
-    
+    int playerMovement = 0;
+    if ( roomstate[1] == 0) {
+        roomstate[1] = 1;
+    }
+    playerMovement = playerDirection(1,1,1,0,player);
+    if (playerMovement == 1) {
+        floor2a(player, roomstate);
+
+    } else if (playerMovement == 2) {
+        floor3(player, roomstate);
+    } else if (playerMovement == 3) {
+        floor3b(player, roomstate);
+    }
 }
 void floor3b(playerInfo &player, int roomstate[8]) {
-    
+    int playerMovement = 0;
+    if ( roomstate[2] == 0) {
+        roomstate[2] = 1;
+    }
+    playerMovement = playerDirection(1,1,1,1,player);
+
+     if (playerMovement == 1) {
+        floor2b(player, roomstate);
+
+    } else if (playerMovement == 2) {
+        floor3(player, roomstate);
+    } else if (playerMovement == 3) {
+        floor3c(player, roomstate);
+    } else if (playerMovement == 4) {
+        floor3a(player, roomstate);
+    }
 }
 void floor3c(playerInfo &player, int roomstate[8]) {
-    
+    int playerMovement = 0;
+    if ( roomstate[3] == 0) {
+        roomstate[3] = 1;
+    }
+    playerMovement = playerDirection(1,1,0,1,player);
+     if (playerMovement == 1) {
+        floor2c(player, roomstate);
+
+    } else if (playerMovement == 2) {
+        floor3(player, roomstate);
+    } else if (playerMovement == 4) {
+        floor3b(player, roomstate);
+    }
 }
 void floor2a(playerInfo &player, int roomstate[8]) {
-    
+    int playerMovement = 0;
+    if ( roomstate[4] == 0) {
+        roomstate[4] = 1;
+    }
+    playerMovement = playerDirection(1,1,1,0,player);
+     if (playerMovement == 1) {
+        floor1(player, roomstate);
+    } else if (playerMovement == 2) {
+        floor3a(player, roomstate);
+    } else if (playerMovement == 3) {
+        floor2b(player, roomstate);
+    } 
 }
 void floor2b(playerInfo &player, int roomstate[8]) {
-    
+    int playerMovement = 0;
+    if ( roomstate[5] == 0) {
+        roomstate[5] = 1;
+    }
+    playerMovement = playerDirection(1,1,1,1,player);
+     if (playerMovement == 1) {
+        floor1(player, roomstate);
+
+    } else if (playerMovement == 2) {
+        floor3b(player, roomstate);
+    } else if (playerMovement == 3) {
+        floor2c(player, roomstate);
+    } else if (playerMovement == 4) {
+        floor2a(player, roomstate);
+    }
 }
 
 // Specification B3 - Add more rooms
 void floor2c(playerInfo &player, int roomstate[8]) {
-    
+    int playerMovement = 0;
+    if ( roomstate[6] == 0) {
+        roomstate[6] = 1;
+    }
+    playerMovement = playerDirection(1,1,0,1,player);
+     if (playerMovement == 1) {
+        floor1(player, roomstate);
+
+    } else if (playerMovement == 2) {
+        floor3c(player, roomstate);
+    } else if (playerMovement == 4) {
+        floor2b(player, roomstate);
+    }
 }
 
 // Specification C1
 void floor1(playerInfo &player, int roomstate[8]) {
+    int playerMovement = 0;
+    if ( roomstate[7] == 0) {
+    roomstate[7] = 1;
+    }
+    playerMovement = playerDirection(1,1,1,1,player);
+     if (playerMovement == 1) {
+        return;
+
+    } else if (playerMovement == 2) {
+        floor2b(player, roomstate);
+    } else if (playerMovement == 3) {
+        floor2c(player, roomstate);
+    } else if (playerMovement == 4) {
+        floor2a(player, roomstate);
+    }
 
 }
+
+int playerDirection(int north, int south, int east, int west, playerInfo &player) {
+    char playerChoice;
+    int directionChosen = 0;
+    
+    while(true) {
+        cout << "Where would you like to move, " << player.name << endl;
+        if(north!=0) {
+            cout << "N - North" << endl;
+        }
+        if(south!=0) {
+            cout << "S - South" << endl;
+            
+        }
+        if(east!=0) {
+            cout << "E - East" << endl;
+            
+        }
+        if(west!=0) {
+            cout << "W - West" << endl;           
+        }
+        cin >> playerChoice;
+        if (tolower(playerChoice)=='n') {
+            directionChosen = 1;
+            break;
+        } else if (tolower(playerChoice)=='s') {
+            directionChosen = 2;
+            break;
+        } else if (tolower(playerChoice)=='e') {
+            directionChosen = 3;
+            break;
+        } else if (tolower(playerChoice)=='w') {
+            directionChosen = 4;
+            break;
+        }
+    }
+    return directionChosen;
+}
+
